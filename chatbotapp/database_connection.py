1d4f5500281ad8c7276806e16bf4111a485f0559
Akshay Syal <61907539+AkshaySyal@users.noreply.github.com>
2025-03-17 02:10:07 -0400

    Update database_connection.py

--- chatbotapp/database_connection.py
+++ chatbotapp/database_connection.py
@@ -1,11 +1,23 @@
+from langchain.sql_database import SQLDatabase
+from dotenv import load_dotenv
+import os
 import sqlite3
+import pandas as pd
 
+#loading environement variables 
+load_dotenv()
+
+#establishing database connection
+def db_conn():
+    sqlite_path = os.getenv('SQLITE_DB_PATH', 'nba_roster_lower_case.db') #default path
+    DB_URL = f"sqlite:///{sqlite_path}"
+    return SQLDatabase.from_uri(DB_URL)
+
+
+#Execute SQL query
 def run_query(sql_query):
-    conn = sqlite3.connect("nba.sqlite")  
-    cursor = conn.cursor()
-    cursor.execute(sql_query)
-    result = cursor.fetchall()
-    columns = [desc[0] for desc in cursor.description] if cursor.description else []
-    conn.commit()
-    conn.close()
-    return result, columns
+    engine = sqlite3.connect(os.getenv('SQLITE_DB_PATH', 'nba_roster_lower_case.db'))
+    return pd.read_sql(sql_query, con=engine).to_string(index=False)
+    # db = db_conn()
+    # with db._engine.connect() as connection:
+    #     return connection.execute("select name from nba_roster where team='Golden State Warriors' and POS='PG';").fetchall()
