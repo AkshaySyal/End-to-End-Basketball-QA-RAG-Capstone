590779aa257ed6816e1a933a5e79ff2b1748efb9
Akshay Syal <61907539+AkshaySyal@users.noreply.github.com>
2025-03-19 12:39:02 -0400

    Changed URL of model

--- chatbotapp/llm_handler.py
+++ chatbotapp/llm_handler.py
@@ -19,7 +19,7 @@ def process_query(question, model_choice):
     return response 
 
 def finetuned_model(question,model='Finetuned-Llama-3.2-3B'):
-    url = "https://ebcd-2601-19b-e02-2da0-ce2-94ff-bb12-1a09.ngrok-free.app/api/generate"
+    url = "https://7b50-2601-19b-e02-2da0-5475-83ca-1abf-127a.ngrok-free.app/api/generate"
     headers = {
         "Content-Type": "application/json",
         "ngrok-skip-browser-warning": "true"  # Add this header
